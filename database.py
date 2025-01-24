b1cfa9713321afe2aea9ccd2ccb7b2d7dd18dd77
CriisGANG <cristofol.comas@gmail.com>
2025-01-24 18:50:02 +0100

    Login Success!

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 import pymysql.cursors
-import sqlalchemy as db
+import sqlalchemy as database
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
-class futbol(object):
+class API_Mensajeria(object):
     def conecta(self):
         # Conexion a la BBDD del servidor mySQL
         self.db = pymysql.connect(host='localhost',
@@ -15,3 +16,23 @@ class futbol(object):
 
     def desconecta(self):
         self.db.close()
+        
+    def carregaUsuaris(self):
+        sql="SELECT * from usuarisclase"
+        self.cursor.execute(sql)
+        ResQuery=self.cursor.fetchall()
+        return ResQuery
+    
+    def verificar_usuario(self, username, password):
+        sql="SELECT count(*) from usuarisclase WHERE username='"+username+"'"
+        self.cursor.execute(sql)
+        ResQuery=self.cursor.fetchone()
+        if ResQuery['count(*)']==1:
+            sql = "SELECT password FROM usuarisclase WHERE username = %s"
+            self.cursor.execute(sql, (username,))
+            ResQuery = self.cursor.fetchone()
+            resposta=check_password_hash(ResQuery['password'],password)
+        else:
+            resposta=False
+        return resposta
+        
