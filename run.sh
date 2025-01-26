ae7ba0ffe060fc35f353844fa57c11cc5b780633
CristinaLopezMontalvo <cristina.lopez.mon@gmail.com>
2025-01-26 23:17:40 +0100

    edit run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+docker build -t api_mensajeria . #construir la imagen
+docker run -d -p 8000:8000 api_mensajeria # ejecutar la imagen en el puerto 
+
+# docker run -> Inicia un nuevo contenedor a partir de una imagen Docker.
+# 
